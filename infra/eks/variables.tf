cc58c5ca9e92b770ea0ee7fed284908554744e9d
kunalkthalautiya <kunalkumarthalautiya@gmail.com>
2026-07-05 15:09:10 +0530

    Add EKS Terraform infra (VPC + EKS cluster + managed node group)

---
+++ infra/eks/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "project3-eks"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.30"
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.small"
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 1
+}
